81ebce067c61bdc9e269765a1bf20aea604c323b
phuchv <141104806+vfa-phuchv@users.noreply.github.com>
2023-09-30 10:10:43 +0700

    first commit

--- firstFile.js
+++ firstFile.js
@@ -1,1 +1,10 @@
 /**This file to test commit with git*/
+
+/** DISCARD CHANGE
+ * - git stash: save changed to stash and reset workspace
+ * - git reset: touch to changed on stage (tracked file)
+ * - git clean: remove every untracked file
+ * 
+ * 
+ * 
+ */
