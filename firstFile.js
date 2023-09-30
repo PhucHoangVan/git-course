45818be9ab3c1e09a747135c9c9dcf4da4d91271
phuchv <141104806+vfa-phuchv@users.noreply.github.com>
2023-09-30 13:31:26 +0700

    eight commit

--- firstFile.js
+++ firstFile.js
@@ -13,6 +13,14 @@
  * 
  */
 
+/** - GIT REBASE
+ * 
+ */
+
+/** - GIT RESTORE           
+ * - git restore
+ * - git restore --staged (đã sử dụng khi loại bỏ conflig chưa được merge)
+ */
 
 /** - GIT AMEND COMMIT
  * 
@@ -29,9 +37,14 @@
  * - git stash pop stash@{1}: apply changed in stash has stash_id = 1 and drop this stash
  * - git stash clear: delete all stash saved in repo
  * 
+ * - git stash list | grep <branch-name>: hiển thị các stash được tạo từ "branch-name"
+ * + Khi dùng git stash để lưu tạm các thay đổi, thì khi apply lại 1 phiên bản stash -> thì tất cả thay đổi được phục hồi lại sẽ được đưa về vùng untracked
+ * + Ngoài ra, còn lệnh git stash save -u (theo lý thuyết: mục đích là để save các file bao gồm "tracked" và "untracked" vào stash, nếu không có "-u" thì chỉ những file "tracked" hay "đã được git add" mới được save vào stash. Tuy nhiên theo thử nghiệm thì không dùng "-u" thì cả "untracked" và "tracked" đều được save và khi phục hồi sẽ phục hồi lại trong vùng "untracked")
  * Q/A:
  * - lệnh git stash list sẽ hiển thị tất cả các stash trên toàn bộ repo hay trên branch hiện tại?
+ * -> Nếu không chỉ định branch thì sẽ lấy stash trên toàn bộ repo
  * - lệnh git apply có thể apply 1 stash được tạo từ brach khác cho branch hiện tại hay không?
+ * -> Có thể, hoàn toàn apply mọi stash được lưu cho mọi branch khác
  */
 
 
