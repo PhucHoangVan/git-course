0e2de85db46f20a5a6ad8f3f16c40ffa57125696
PhucHoangVan <20521760@gm.uit.edu.vn>
2023-10-01 22:43:40 +0700

    test git revert

---
+++ git-revert.js
@@ -0,0 +1,10 @@
+/**
+ 
+- git reset (compare diff ở giữa 2 commit)
+-> a - b - c - d
+=> reset từ d->a
+=> diff = b,c
+
+- git revert (compare change tại current commit vs commit đứng liền trước commit đang destinate <tức là không so sánh với change trong commit destinate, mà so sánh với các change trước khi commit destinate được commit>)
+ 
+*/
