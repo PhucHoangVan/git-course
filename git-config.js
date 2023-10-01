912bc6d9bbf3114034e02b08dd14a21cf65298e3
PhucHoangVan <20521760@gm.uit.edu.vn>
2023-10-01 17:56:15 +0700

    git config knowledge

---
+++ git-config.js
@@ -0,0 +1,16 @@
+/**
+ * @_GIT_CONFIG
+ * 
+ * @Description
+ * !Git log is used for case want to change account git used
+ * 
+ * - git config --list: Xem cấu hình git trên local
+ * - git config user.name: Xem thông tin username
+ * - git config user.email: Xem thông tin email
+ * 
+ * todo: Thay đổi thông tin tài khoản git như sau:
+ * - git config --global user.name "your-username"
+ * - git config --global user.email "your-email@gmail.com"
+ * 
+ * ? Một file: <~/.gitconfig> trên device sẽ chứa các thông tin cấu hình git
+ */
